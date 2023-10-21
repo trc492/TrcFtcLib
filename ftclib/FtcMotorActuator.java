71f00569fbbe4c8dcc41d27d1f57d5a805622deb
Titan Robotics Club <trc492@users.noreply.github.com>
2023-10-20 19:14:24 -0700

    Added support for external enocder and slave motor for FtcMotorActuator.

--- ftclib/FtcMotorActuator.java
+++ ftclib/FtcMotorActuator.java
@@ -26,26 +26,32 @@ import java.util.Arrays;
 import java.util.Locale;
 
 import TrcCommonLib.trclib.TrcDbgTrace;
+import TrcCommonLib.trclib.TrcMotor;
 import TrcCommonLib.trclib.TrcRobotBattery;
 import TrcCommonLib.trclib.TrcUtil;
 
 /**
- * This class implements a platform dependent motor actuator. A motor actuator consists of a DC motor, a lower limit
- * switch and an optional upper limit switch. It creates all the necessary components for a PID controlled actuator
- * which could include a software PID controller.
+ * This class implements a platform dependent motor actuator. A motor actuator consists of a DC motor or a continuous
+ * rotation servo, optionally a lower limit switch, an upper limit switch and an encoder. It creates all the necessary
+ * components for a PID controlled actuator which could include a software PID controller.
  */
 public class FtcMotorActuator
 {
     /**
-     * This class contains all the parameters related to the actuator motor.
+     * This class contains all the parameters related to the actuator.
      */
-    public static class MotorParams
+    public static class Params
     {
         public boolean motorInverted = false;
-        public boolean lowerLimitSwitchEnabled = false;
+        public boolean hasSlaveMotor = false;
+        public boolean slaveMotorInverted = false;
+        public boolean hasLowerLimitSwitch = false;
         public boolean lowerLimitSwitchInverted = false;
-        public boolean upperLimitSwitchEnabled = false;
+        public boolean hasUpperLimitSwitch = false;
         public boolean upperLimitSwitchInverted = false;
+        public boolean hasExternalEncoder = false;
+        public boolean encoderInverted = false;
+        public boolean encoderAbsolute = false;
         public double positionScale = 1.0;
         public double positionOffset = 0.0;
         public double[] positionPresets = null;
@@ -57,39 +63,69 @@ public class FtcMotorActuator
          * @param inverted specifies true to invert motor direction, false otherwise.
          * @return this object for chaining.
          */
-        public MotorParams setMotorInverted(boolean inverted)
+        public Params setMotorInverted(boolean inverted)
         {
             motorInverted = inverted;
             return this;
         }   //setMotorInverted
 
         /**
+         * This methods sets the slave motor if there is one and also sets its direction.
+         *
+         * @param hasSlaveMotor specifies true if there is a slave motor, false otherwise.
+         * @param inverted specifies true to invert motor direction, false otherwise.
+         * @return this object for chaining.
+         */
+        public Params setSlaveMotor(boolean hasSlaveMotor, boolean inverted)
+        {
+            this.hasSlaveMotor = hasSlaveMotor;
+            this.slaveMotorInverted = inverted;
+            return this;
+        }   //setSlaveMotor
+
+        /**
          * This method sets the lower limit switch properties.
          *
-         * @param enabled specifies true if there is a lower limit switch, false if none.
+         * @param hasLimitSwitch specifies true if there is a lower limit switch, false otherwise.
          * @param inverted specifies true if the limit switch is normally open, false if normally close.
          * @return this object for chaining.
          */
-        public MotorParams setLowerLimitSwitchEnabled(boolean enabled, boolean inverted)
+        public Params setLowerLimitSwitch(boolean hasLimitSwitch, boolean inverted)
         {
-            lowerLimitSwitchEnabled = enabled;
+            hasLowerLimitSwitch = hasLimitSwitch;
             lowerLimitSwitchInverted = inverted;
             return this;
-        }   //setLowerLimitSwitchEnabled
+        }   //setLowerLimitSwitch
 
         /**
          * This method sets the upper limit switch properties.
          *
-         * @param enabled specifies true if there is an upper limit switch, false if none.
+         * @param hasLimitSwitch specifies true if there is an upper limit switch, false otherwise.
          * @param inverted specifies true if the limit switch is normally open, false if normally close.
          * @return this object for chaining.
          */
-        public MotorParams setUpperLimitSwitchEnabled(boolean enabled, boolean inverted)
+        public Params setUpperLimitSwitch(boolean hasLimitSwitch, boolean inverted)
         {
-            upperLimitSwitchEnabled = enabled;
+            hasUpperLimitSwitch = hasLimitSwitch;
             upperLimitSwitchInverted = inverted;
             return this;
-        }   //setUpperLimitSwitchEnabled
+        }   //setUpperLimitSwitch
+
+        /**
+         * This method sets whether the actuator has an external encoder.
+         *
+         * @param hasEncoder specifies true if there is an external encoder, false otherwise.
+         * @param inverted specifies true if the encoder is inverted, false otherwise.
+         * @param absolute specifies true if the encoder is absolute, false otherwise.
+         * @return this object for chaining.
+         */
+        public Params setExternalEncoder(boolean hasEncoder, boolean inverted, boolean absolute)
+        {
+            hasExternalEncoder = hasEncoder;
+            encoderInverted = inverted;
+            encoderAbsolute = absolute;
+            return this;
+        }   //setExternalEncoder
 
         /**
          * This method sets the position sensor scale factor and offset.
@@ -98,7 +134,7 @@ public class FtcMotorActuator
          * @param offset specifies offset added to the scaled sensor reading.
          * @return this object for chaining.
          */
-        public MotorParams setPositionScaleAndOffset(double scale, double offset)
+        public Params setPositionScaleAndOffset(double scale, double offset)
         {
             positionScale = scale;
             positionOffset = offset;
@@ -112,7 +148,7 @@ public class FtcMotorActuator
          * @param posPresets specifies an array of preset positions in scaled unit.
          * @return this object for chaining.
          */
-        public MotorParams setPositionPresets(double tolerance, double... posPresets)
+        public Params setPositionPresets(double tolerance, double... posPresets)
         {
             positionPresets = posPresets;
             positionPresetTolerance = tolerance;
@@ -120,60 +156,92 @@ public class FtcMotorActuator
         }   //setPositionPresets
 
         /**
-         * This method returns the string format of the motorParams info.
+         * This method returns the string format of the Params info.
          *
-         * @return string format of the motor param info.
+         * @return string format of the params info.
          */
         @Override
         public String toString()
         {
             return String.format(
                 Locale.US,
-                "motorInverted=%s,lowerLimitEnabled=%s,lowerLimitInverted=%s," +
-                "upperLimitEnabled=%s,upperLimitInverted=%s,scale=%f,offset=%f,presetTolerance=%f,presets=%s",
-                motorInverted, lowerLimitSwitchEnabled, lowerLimitSwitchInverted,
-                upperLimitSwitchEnabled, upperLimitSwitchInverted, positionScale, positionOffset,
-                positionPresetTolerance, Arrays.toString(positionPresets));
+                "motorInverted=%s,hasSlave=%s,slaveInverted=%s,hasLowerLimit=%s,lowerLimitInverted=%s," +
+                "hasUpperLimit=%s,upperLimitInverted=%s,hasEncoder=%s,encoderInverted=%s, encoderAbs=%s" +
+                "scale=%f,offset=%f,presetTolerance=%f,presets=%s",
+                motorInverted, hasSlaveMotor, slaveMotorInverted, hasLowerLimitSwitch, lowerLimitSwitchInverted,
+                hasUpperLimitSwitch, upperLimitSwitchInverted, hasExternalEncoder, encoderInverted, encoderAbsolute,
+                positionScale, positionOffset, positionPresetTolerance, Arrays.toString(positionPresets));
         }   //toString
 
-    }   //class MotorParams
+    }   //class Params
 
     protected final String instanceName;
-    protected final FtcDcMotor actuator;
+    protected final TrcMotor actuator;
 
     /**
      * Constructor: Create an instance of the object.
      *
      * @param instanceName specifies the instance name.
-     * @param motorParams specifies the parameters to set up the actuator motor.
+     * @param isCRServo specifies true if motor is a continuous rotation servo, false if it is a DC Motor.
+     * @param params specifies the parameters to set up the actuator.
      * @param tracer specifies the tracer for debug tracing, can be null if not provided.
      * @param tracePidInfo specifies true to debug PID, ignore if tracer is null.
      * @param battery specifies the battery object for tracing battery level, ignore if tracer is null.
      */
     public FtcMotorActuator(
-        String instanceName, MotorParams motorParams, TrcDbgTrace tracer, boolean tracePidInfo, TrcRobotBattery battery)
+        String instanceName, boolean isCRServo, Params params, TrcDbgTrace tracer, boolean tracePidInfo,
+        TrcRobotBattery battery)
     {
         FtcDigitalInput lowerLimitSwitch =
-            motorParams.lowerLimitSwitchEnabled? new FtcDigitalInput(instanceName + ".lowerLimit"): null;
+            params.hasLowerLimitSwitch? new FtcDigitalInput(instanceName + ".lowerLimit"): null;
         FtcDigitalInput upperLimitSwitch =
-            motorParams.upperLimitSwitchEnabled? new FtcDigitalInput(instanceName + ".upperLimit"): null;
+            params.hasUpperLimitSwitch? new FtcDigitalInput(instanceName + ".upperLimit"): null;
+        FtcAnalogEncoder encoder =
+            params.hasExternalEncoder? new FtcAnalogEncoder(instanceName + ".encoder"): null;
+
+        if (encoder != null)
+        {
+            encoder.setInverted(params.encoderInverted);
+            if (params.encoderAbsolute)
+            {
+                // Enable cartesian converter for absolute encoder.
+                encoder.setEnabled(true);
+            }
+        }
 
         this.instanceName = instanceName;
-        actuator = new FtcDcMotor(instanceName + ".motor", lowerLimitSwitch, upperLimitSwitch);
-        if (motorParams.lowerLimitSwitchEnabled)
+        actuator =
+            isCRServo? new FtcCRServo(instanceName + ".servo", lowerLimitSwitch, upperLimitSwitch, encoder):
+                       new FtcDcMotor(instanceName + ".motor", lowerLimitSwitch, upperLimitSwitch, encoder);
+
+        if (params.hasSlaveMotor)
         {
-            actuator.enableLowerLimitSwitch(motorParams.lowerLimitSwitchInverted);
+            TrcMotor slave =
+                isCRServo? new FtcCRServo(instanceName + ".slaveServo"): new FtcDcMotor(instanceName + ".slaveMotor");
+            slave.setMotorInverted(params.slaveMotorInverted);
+            slave.follow(actuator);
         }
-        if (motorParams.upperLimitSwitchEnabled)
+
+        if (lowerLimitSwitch != null)
         {
-            actuator.enableUpperLimitSwitch(motorParams.upperLimitSwitchInverted);
+            actuator.enableLowerLimitSwitch(params.lowerLimitSwitchInverted);
         }
-        actuator.setBrakeModeEnabled(true);
+
+        if (upperLimitSwitch != null)
+        {
+            actuator.enableUpperLimitSwitch(params.upperLimitSwitchInverted);
+        }
+
+        if (!isCRServo)
+        {
+            actuator.setBrakeModeEnabled(true);
+        }
+
         actuator.setOdometryEnabled(true, true, true);
-        actuator.setMotorInverted(motorParams.motorInverted);
+        actuator.setMotorInverted(params.motorInverted);
         actuator.setVoltageCompensationEnabled(TrcUtil.BATTERY_NOMINAL_VOLTAGE);
-        actuator.setPositionSensorScaleAndOffset(motorParams.positionScale, motorParams.positionOffset);
-        actuator.setPosPresets(motorParams.positionPresetTolerance, motorParams.positionPresets);
+        actuator.setPositionSensorScaleAndOffset(params.positionScale, params.positionOffset);
+        actuator.setPosPresets(params.positionPresetTolerance, params.positionPresets);
         actuator.setMsgTracer(tracer, tracePidInfo, battery);
     }   //FtcMotorActuator
 
@@ -181,24 +249,39 @@ public class FtcMotorActuator
      * Constructor: Create an instance of the object.
      *
      * @param instanceName specifies the instance name.
-     * @param motorParams specifies the parameters to set up the actuator motor.
+     * @param isCRServo specifies true if motor is a continuous rotation servo, false if it is a DC Motor.
+     * @param params specifies the parameters to set up the actuator.
      * @param tracer specifies the tracer for debug tracing, can be null if not provided.
      * @param tracePidInfo specifies true to debug PID, ignore if tracer is null.
      */
-    public FtcMotorActuator(String instanceName, MotorParams motorParams, TrcDbgTrace tracer, boolean tracePidInfo)
+    public FtcMotorActuator(
+        String instanceName, boolean isCRServo, Params params, TrcDbgTrace tracer, boolean tracePidInfo)
     {
-        this(instanceName, motorParams, tracer, tracePidInfo, null);
+        this(instanceName, isCRServo, params, tracer, tracePidInfo, null);
     }   //FtcMotorActuator
 
     /**
      * Constructor: Create an instance of the object.
      *
      * @param instanceName specifies the instance name.
-     * @param motorParams specifies the parameters to set up the actuator motor.
+     * @param params specifies the parameters to set up the actuator.
+     * @param tracer specifies the tracer for debug tracing, can be null if not provided.
+     * @param tracePidInfo specifies true to debug PID, ignore if tracer is null.
      */
-    public FtcMotorActuator(String instanceName, MotorParams motorParams)
+    public FtcMotorActuator(String instanceName, Params params, TrcDbgTrace tracer, boolean tracePidInfo)
     {
-        this(instanceName, motorParams, null, false, null);
+        this(instanceName, false, params, tracer, tracePidInfo, null);
+    }   //FtcMotorActuator
+
+    /**
+     * Constructor: Create an instance of the object.
+     *
+     * @param instanceName specifies the instance name.
+     * @param params specifies the parameters to set up the actuator.
+     */
+    public FtcMotorActuator(String instanceName, Params params)
+    {
+        this(instanceName, false, params, null, false, null);
     }   //FtcMotorActuator
 
     /**
@@ -213,14 +296,13 @@ public class FtcMotorActuator
     }   //toString
 
     /**
-     * This method returns the actuator motor object.
+     * This method returns the actuator object.
      *
-     * @return actuator motor object.
+     * @return actuator object.
      */
-    public FtcDcMotor getMotor()
+    public TrcMotor getActuator()
     {
         return actuator;
-    }   //getMotor
+    }   //getActuator
 
 }   //class FtcMotorActuator
-
